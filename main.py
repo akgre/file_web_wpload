9c06a58af8331cdb5cc32e093d32368a9ce43f45
Zircon <akgreenyer@gmail.com>
2022-08-31 23:11:40 +0200

    - index

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="my test app")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get('/index/', response_class=HTMLResponse)
+@app.get('/', response_class=HTMLResponse)
 def index(request: Request):
     context = {"request": request}
     return templates.TemplateResponse("index.html", context)
