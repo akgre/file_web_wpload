a1d80894404052d77c57f5d20a97b936eb0a1cc0
Zircon <akgreenyer@gmail.com>
2022-09-01 06:08:05 +0200

    - upload file

--- main.py
+++ main.py
@@ -31,6 +31,6 @@ async def create_item(name: str):
 #     return {"file_size": len(file)}
 
 
-# @app.post("/uploadfile/")
-# async def create_upload_file(file: UploadFile):
-#     return {"filename": file.filename}
+@app.post("/uploadfile")
+def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
